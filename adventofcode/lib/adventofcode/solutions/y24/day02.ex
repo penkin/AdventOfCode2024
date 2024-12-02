82be7748d771504603ef11c020be1a4888824093
Christopher Penkin <chris@penkin.me>
2024-12-02 21:31:40 +0200

    Day 2 solutions.

--- adventofcode/lib/adventofcode/solutions/y24/day02.ex
+++ adventofcode/lib/adventofcode/solutions/y24/day02.ex
@@ -2,33 +2,65 @@ defmodule AdventOfCode.Solutions.Y24.Day02 do
   alias AoC.Input
 
   def parse(input, _part) do
-    # This function will receive the input path or an %AoC.Input.TestInput{}
-    # struct. To support the test you may read both types of input with either:
-    #
-    # * Input.stream!(input), equivalent to File.stream!/1
-    # * Input.stream!(input, trim: true), equivalent to File.stream!/2
-    # * Input.read!(input), equivalent to File.read!/1
-    #
-    # The role of your parse/2 function is to return a "problem" for the solve/2
-    # function.
-    #
-    # For instance:
-    #
-    # input
-    # |> Input.stream!()
-    # |> Enum.map!(&my_parse_line_function/1)
-
     Input.read!(input)
+    |> String.split("\n")
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(fn floor ->
+      floor
+      |> String.split(" ")
+      |> Enum.map(&String.to_integer/1)
+    end)
   end
 
   def part_one(problem) do
-    # This function receives the problem returned by parse/2 and must return
-    # today's problem solution for part one.
-
     problem
+    |> Enum.count(&check_safe/1)
   end
 
-  # def part_two(problem) do
-  #   problem
-  # end
+  def part_two(problem) do
+      problem
+    |> Enum.count(&make_safe/1)
+  end
+
+  defp check_safe(floor) do
+    case floor do
+      [] -> true
+      [_] -> true
+      [a, b | rest] when a > b -> check_safe_down([a, b | rest])
+      [a, b | rest] when a < b -> check_safe_up([a, b | rest])
+      _ -> false
+    end
+  end
+
+  defp check_safe_up(floor) do
+    case floor do
+      [_] -> true
+      [a, b | rest] when (b - a) in 1..3 -> check_safe_up([b | rest])
+      _ -> false
+    end
+  end
+
+  defp check_safe_down(floor) do
+    case floor do
+      [_] -> true
+      [a, b | rest] when (a - b) in 1..3 ->
+        check_safe_down([b | rest]) 
+      _ -> false
+    end
+  end
+
+  defp make_safe(floor) do
+    if check_safe(floor) do
+      true
+    else
+      floor
+        |>Enum.with_index()
+        |>Enum.any?(fn {_, index} -> 
+          floor
+          |>List.delete_at(index)
+          |>check_safe()
+        end)
+    end
+  end
+
 end
--- adventofcode/test/2024/day02_test.exs
+++ adventofcode/test/2024/day02_test.exs
@@ -3,23 +3,6 @@ defmodule AdventOfCode.Solutions.Y24.Day02Test do
   alias AdventOfCode.Solutions.Y24.Day02, as: Solution, warn: false
   use ExUnit.Case, async: true
 
-  # To run the test, run one of the following commands:
-  #
-  #     mix AoC.test --year 2024 --day 2
-  #
-  #     mix test test/2024/day02_test.exs
-  #
-  # To run the solution
-  #
-  #     mix AoC.run --year 2024 --day 2 --part 1
-  #
-  # Use sample input file:
-  #
-  #     # returns {:ok, "priv/input/2024/day-02-mysuffix.inp"}
-  #     {:ok, path} = Input.resolve(2024, 2, "mysuffix")
-  #
-  # Good luck!
-
   defp solve(input, part) do
     problem =
       input
@@ -31,43 +14,39 @@ defmodule AdventOfCode.Solutions.Y24.Day02Test do
 
   test "part one example" do
     input = ~S"""
-    This is an
-    example input.
-    replace with
-    an example from
-    the AoC website.
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
     """
 
-    assert CHANGE_ME == solve(input, :part_one)
+    assert 2 == solve(input, :part_one)
   end
 
-  # Once your part one was successfully sumbitted, you may uncomment this test
-  # to ensure your implementation was not altered when you implement part two.
+  @part_one_solution 472
 
-  # @part_one_solution CHANGE_ME
-  #
-  # test "part one solution" do
-  #   assert {:ok, @part_one_solution} == AoC.run(2024, 2, :part_one)
-  # end
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2024, 2, :part_one)
+  end
 
-  # test "part two example" do
-  #   input = ~S"""
-  #   This is an
-  #   example input.
-  #   replace with
-  #   an example from
-  #   the AoC website.
-  #   """
-  #
-  #   assert CHANGE_ME == solve(input, :part_two)
-  # end
+  test "part two example" do
+    input = ~S"""
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
+    """
 
-  # You may also implement a test to validate the part two to ensure that you
-  # did not broke your shared modules when implementing another problem.
+    assert 4 == solve(input, :part_two)
+  end
 
-  # @part_two_solution CHANGE_ME
-  #
-  # test "part two solution" do
-  #   assert {:ok, @part_two_solution} == AoC.run(2024, 2, :part_two)
-  # end
+  @part_two_solution 520
+
+  test "part two solution" do
+    assert {:ok, @part_two_solution} == AoC.run(2024, 2, :part_two)
+  end
 end
